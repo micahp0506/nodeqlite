deb945f99d660f6ad376790ed6143367ffe44d56
Micah Wells <micahp0506@gmail.com>
2016-02-25 14:50:13 -0600

    Working with SQL queries and NODE

--- app.js
+++ app.js
@@ -1,17 +1,77 @@
 'use strict';
 
 
+const express = require('express');
+const app = express();
 const sqlite3 = require('sqlite3');
 const db = new sqlite3.Database('./db/Chinook_Sqlite.sqlite');
+const PORT = process.env.PORT || 3000;
 
 // Getting the # of Invoices per country and ordering them in Descending order
-console.log("# of invoices per Country");
-db.all(`SELECT COUNT(*) as Count,
-            BillingCountry as Country
-            FROM Invoice GROUP BY BillingCountry
-            ORDER BY Count DESC`,
-                (err, res) => {
-                    if (err) throw (err);
+app.get ('/invoices-per-country', (req, res) => {
+    db.all(
+        `SELECT COUNT(*) as Count,
+        BillingCountry as Country
+        FROM Invoice GROUP BY BillingCountry
+        ORDER BY Count DESC`,
+            (err, data) => {
+                if (err) throw (err);
 
-                    console.log(res);
+                res.send({
+                    data: data,
+                    info: '# of invoices per country'
+                });
+            }
+    );
 });
+
+
+app.get('/sales-per-year', (req, res) => {
+  // How many Invoices were there in 2009 and 2011? What are the respective total sales for each of those years?
+  //req.query = { filter: { year: '2009,2011' } }
+
+  let having = '';
+
+  if (req.query.filter) {
+    having = 'HAVING';
+
+    // Taking the query paramaters from the URL and making them an array ignoring the comma, then using map to make them an integer, having is the string that we are building for the SQL query
+    req.query.filter.year
+      .split(',')
+      .map(y => +y)
+      .forEach(y => {
+        having += ` year = "${y}" OR`;
+      });
+
+    having = having.substring(0, having.length - 3);
+  }
+
+  db.all(`
+    SELECT count(*) as invoices,
+           sum(Total) as total,
+           strftime('%Y', InvoiceDate) as year
+    FROM   Invoice
+    GROUP BY year
+    ${having}`,
+      (err, data) => {
+        if (err) throw err;
+
+        const roundedData = data.map(function (obj) {
+          return {
+            invoices: obj.invoices,
+            year: +obj.year,
+            total: +obj.total.toFixed(2)
+          }
+        });
+
+        res.send({
+          data: roundedData,
+          info: '# of invoices and sales per year'
+        });
+      }
+    );
+});
+
+app.listen(PORT, () => {
+    console.log(`App listening on ${PORT}`);
+})
